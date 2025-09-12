ca053bccaa3683c350ae07ca0c40fa46102757da
anjujaiswal <jaiswalanju2019@gmail.com>
2025-09-12 21:58:23 +0530

    Fetching user cart

--- src/main/java/com/ecommerce/project/contoller/CartController.java
+++ src/main/java/com/ecommerce/project/contoller/CartController.java
@@ -1,7 +1,12 @@
 package com.ecommerce.project.contoller;
 
+import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.repositories.CartRepository;
 import com.ecommerce.project.service.CartService;
+import com.ecommerce.project.util.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class CartController {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
 
     @Autowired
     private CartService cartService;
@@ -27,5 +38,14 @@ public class CartController {
     public ResponseEntity<List<CartDTO>> getCarts(){
         List<CartDTO> cartDTOS = cartService.getAllCarts();
         return new ResponseEntity<List<CartDTO>>(cartDTOS, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/carts/user/cart")
+    public ResponseEntity<CartDTO> getCartById(){
+        String emailId = authUtil.loggedInEmail();
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        Long cartId = cart.getCartId();
+        CartDTO cartDTO = cartService.getCart(emailId, cartId);
+        return new ResponseEntity<>(cartDTO, HttpStatus.OK);
     }
 }
